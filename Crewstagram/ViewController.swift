94460fe7a14a382739fc2e5e6b4f715bc5faf066
Kerry <mrkerrywashington@icloud.com>
2017-10-06 22:30:06 -0700

    Initial Commit

---
+++ Crewstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crewstagram
+//
+//  Created by Kerry Washington on 10/6/17.
+//  Copyright © 2017 Grunt Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
